51a9598d8092c8d5d5d866c2dd4827e059183232
Patrick Henninger <patrick.henninger@contagt.com>
2021-12-15 03:32:35 +0100

    Lint

--- src/commands/resources/update.ts
+++ src/commands/resources/update.ts
@@ -1,5 +1,5 @@
 import {Command, flags} from '@oclif/command'
-//import ResourceManager from '../../core/resources/resource-manager'
+// import ResourceManager from '../../core/resources/resource-manager'
 
 export default class ResourcesUpdate extends Command {
   static description = 'Update resources in resources.json to their latest version based on the strategy'
